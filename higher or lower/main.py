003cbcf1663512189433eadb657962e8ccdd790a
Huy Nguyen <ngmanhhuy@users.noreply.github.com>
2024-10-03 21:48:02 +0000

    more fix for duplicate and format

--- higher or lower/main.py
+++ higher or lower/main.py
@@ -3,17 +3,15 @@ from art import logo, vs
 from game_data import data
 
 def print_choices(a, b):
-    print(f"Compare A: {a.get('name')}, {a.get('description')}, from {a.get('country')}")
+    print(f"Compare A: {a['name']}, {a['description']}, from {a['country']}")
     print(vs)
-    print(f"Against B: {b.get('name')}, {b.get('description')}, from {b.get('country')}")
+    print(f"Against B: {b['name']}, {b['description']}, from {b['country']}")
 
 def is_correct(a, b, choice):
-    if a.get('follower_count') > b.get('follower_count') and choice == 'a':
-        return True
-    elif a.get('follower_count') < b.get('follower_count') and choice == 'b':
-        return True
+    if a['follower_count'] > b['follower_count']:
+        return choice == 'a'
     else:
-        return False
+        return choice == 'b'
 
 def higher_or_lower():
     print(logo)
@@ -21,7 +19,7 @@ def higher_or_lower():
     score = 0
     choice_a = random.choice(data)
     choice_b = random.choice(data)
-    if choice_a == choice_b:
+    while choice_b == choice_a:
         choice_b = random.choice(data)
 
     while not game_over:
@@ -33,15 +31,15 @@ def higher_or_lower():
             print(f"Correct! Your current score: {score}")
             if user_choice == 'b':
                 choice_a = choice_b
-            choice_b = random.choice(data)
-            if choice_a == choice_b:
+            current_b = choice_b
+            while choice_b == current_b or choice_b == choice_a:
                 choice_b = random.choice(data)
         else:
             game_over = True
 
     if game_over:
-        print(f"Oops... You lose! Your score is: {score}")
-        print(f"{choice_a.get('name')} has {choice_a.get('follower_count')} million followers")
-        print(f"{choice_b.get('name')} has {choice_b.get('follower_count')} million followers")
+        print(f"Oops... You lose! Final score is: {score}")
+        print(f"{choice_a['name']} has {choice_a['follower_count']} million followers")
+        print(f"{choice_b['name']} has {choice_b['follower_count']} million followers")
 
 higher_or_lower()
