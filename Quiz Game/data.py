01d9c03c92fe87f73c5aea99a56aec9cfa005ba2
Huy Nguyen <ngmanhhuy@users.noreply.github.com>
2024-10-09 01:53:08 +0000

    Quiz Game

---
+++ Quiz Game/data.py
@@ -0,0 +1,82 @@
+question_data = [
+        {
+            "type": "boolean",
+            "difficulty": "medium",
+            "category": "General Knowledge",
+            "question": "The scientific name for the Southern Lights is Aurora Australis?",
+            "correct_answer": "True",
+            "incorrect_answers": ["False"]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "easy",
+            "category": "General Knowledge",
+            "question": "Jingle Bells was originally meant for Thanksgiving",
+            "correct_answer": "True",
+            "incorrect_answers": ["False"]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "medium",
+            "category": "General Knowledge",
+            "question": "There are 86400 seconds in a day.",
+            "correct_answer": "True",
+            "incorrect_answers": ["False"]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "hard",
+            "category": "General Knowledge",
+            "question": "Spoon theory is a theory, utilizing &quot;Spoons&quot; as a metaphor for energy they can use in a day.",
+            "correct_answer": "True",
+            "incorrect_answers": ["False"]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "easy",
+            "category": "General Knowledge",
+            "question": "Adolf Hitler was born in Australia.",
+            "correct_answer": "False",
+            "incorrect_answers": ["True"]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "easy",
+            "category": "General Knowledge",
+            "question": "&quot;Ananas&quot; is mostly used as the word for Pineapple in other languages.",
+            "correct_answer": "True",
+            "incorrect_answers": ["False"]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "easy",
+            "category": "General Knowledge",
+            "question": "The mitochondria is the powerhouse of the cell.",
+            "correct_answer": "True",
+            "incorrect_answers": ["False"]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "easy",
+            "category": "General Knowledge",
+            "question": "Nutella is produced by the German company Ferrero.",
+            "correct_answer": "False",
+            "incorrect_answers": ["True"]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "medium",
+            "category": "General Knowledge",
+            "question": "Furby was released in 1998.",
+            "correct_answer": "True",
+            "incorrect_answers": ["False"]
+        },
+        {
+            "type": "boolean",
+            "difficulty": "medium",
+            "category": "General Knowledge",
+            "question": "&quot;Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo.&quot; is a grammatically correct sentence.",
+            "correct_answer": "True",
+            "incorrect_answers": ["False"]
+        }
+]
